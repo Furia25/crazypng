d86058b4564b521ab5fc13991e393c6f5255ac2e
furia25 <val@ValPc.>
2025-04-29 01:00:02 +0200

    Added (crazypng.h) : Chunks structures and others structures

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # crazypng
 C png decoder
+
+[text](http://www.libpng.org/pub/png/pngsuite.html)
+[text](https://www.w3.org/TR/PNG-Chunks.html)
+[text](http://www.libpng.org/pub/png/spec/1.1/PNG-Chunks.html)
--- crazypng.h
+++ crazypng.h
@@ -10,3 +10,69 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CRAZYPNG_H
+# define CRAZYPNG_H
+# include <stdint.h>
+# include <stdbool.h>
+
+# define PNG_CHUNK_TYPE_CHAR_IHDR "IHDR"
+# define PNG_CHUNK_TYPE_CHAR_IDAT "IDAT"
+# define PNG_CHUNK_TYPE_CHAR_IEND "IEND"
+# define PNG_CHUNK_TYPE_CHAR_PLTE "PLTE"
+
+typedef enum e_png_chunk_type
+{
+	PNG_CHUNK_IHDR,
+	PNG_CHUNK_IDAT,
+	PNG_CHUNK_IEND,
+	PNG_CHUNK_PLTE
+}	t_png_chunk_type;
+
+typedef enum e_png_color_type
+{
+	PNG_GRAYSCALE		= 0,
+	PNG_RGB				= 2,
+	PNG_PALETTE			= 3,
+	PNG_ALPHA_GRAYSCALE = 4,
+	PNG_RGBA			= 6
+}	t_png_color_type;
+
+typedef struct s_png_chunk_data_IHDR
+{
+	uint32_t	width;
+	uint32_t	height;
+	uint8_t		bit_depth;
+	uint8_t		color_type;
+	uint8_t		compression;
+	uint8_t		filter;
+	uint8_t		interlace;
+}	t_png_chunk_data_IHDR;
+
+typedef struct s_png_chunk_header
+{
+	uint32_t			length;
+	t_png_chunk_type	type;
+}	t_png_chunk_header;
+
+typedef struct s_png_chunk
+{
+	t_png_chunk_header	header;
+	uint8_t				*data;
+	uint32_t			checksum;
+}	t_png_chunk;
+
+typedef struct s_png
+{
+	//	image_header
+	uint32_t			width;
+	uint32_t			height;
+	uint8_t				bit_depth;
+	t_png_color_type	color_type;
+	bool				interlace;
+	//
+	u
+}	t_png;
+
+uint32_t	swap_endian(uint32_t value);
+
+#endif
